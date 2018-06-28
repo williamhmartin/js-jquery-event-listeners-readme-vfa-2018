883fdbd36185c2e5590cdaffb450a0e19bcc19b0
William Martin <willhuntermartin@gmail.com>
2018-06-28 20:08:57 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,10 @@
 //define functions here
+$('p').on("click", function getIt(){
+    alert('Hey!')
+});
+$('img.tasty').on("load", function frameIt(){
 
+});
 $(document).ready(function(){
 
 // call functions here
