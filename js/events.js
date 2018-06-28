77d7343e03a676875b8292523e1b35e93d26914d
William Martin <willhuntermartin@gmail.com>
2018-06-28 20:19:57 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -9,7 +9,14 @@ $(document).on("keydown", function pressIt(){
   if(key.which == 71){
       alert('g was pressed');
   }
-  
+  $("form").on("submit", function submitIt() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+    return;
+  }
+  alert("you entered the wrong value");
+  return;
+});
 // call functions here
 
 });
