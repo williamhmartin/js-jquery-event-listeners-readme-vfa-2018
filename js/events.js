8c50700a5e5cc33b2ff38b4df3bf99a27322ef76
William Martin <willhuntermartin@gmail.com>
2018-06-28 20:20:56 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,14 +1,18 @@
 //define functions here
+
 $('p').on("click", function getIt(){
     alert('Hey!')
 });
+
 $('img').on("load", function frameIt(){
     'img.tasty'
 });
+
 $(document).on("keydown", function pressIt(){
   if(key.which == 71){
       alert('g was pressed');
   }
+  
   $("form").on("submit", function submitIt() {
   if ($( "input:first" ).val() === "correct") {
     alert('your form is going to be submitted now');
